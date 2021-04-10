382772746c820513426a7cd65ac609b2db5c0418
loadcode229 <sepone229@gmail.com>
2021-04-10 19:29:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Arrow Left") {
+        moveDodgerLeft();
+    } else if (e.key === "Arrow Right") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+};
